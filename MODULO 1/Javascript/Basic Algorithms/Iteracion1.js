7a8b1ec439549e8aafd718d9a95d225a43356995
Sergiolmos4 <olmos5454@gmail.com>
2024-03-19 17:11:18 +0100

    ejercicios 19/03

---
+++ MODULO 1/Javascript/Basic Algorithms/Iteracion1.js
@@ -0,0 +1,14 @@
+//Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella//
+// Crea una variable llamada x, asigna el valor 50 a ella.//
+// Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.//
+// Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.//
+let myFavoriteHero = "Hulk";
+console.log("mi primer consolelog, myFavoriteHero");
+let x = 50;
+console.log(x);
+let h = 5;
+console.log(h);
+let y = 10;
+console.log(y);
+let z = h + y;
+console.log(z);
