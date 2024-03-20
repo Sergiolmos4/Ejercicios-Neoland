b3d36eebba0402c06eca9c83fc382eb038102c96
Sergiolmos4 <olmos5454@gmail.com>
2024-03-20 17:28:37 +0100

    20/03

--- MODULO 1/Javascript/Basic Algorithms/Iteracion2.js
+++ MODULO 1/Javascript/Basic Algorithms/Iteracion2.js
@@ -1,13 +1,14 @@
 // 1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
 const character = { name: "Jack Sparrow", age: 14 };
+character.age = 25;
+console.log(character);
 // 1.2 Declara 3 variables con los nombres y valores siguientes
-let firstName = "Jon";
-let lastName = "Snow";
-let age = 25;
+const firstName = "Jon";
+const lastName = "Snow";
+const age = 24;
 // Muestralos por consola de esta forma: 'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
-console.log(
-  "Soy ${firstName} $ {lastName} tengo ${age} años y me gustan los lobos"
-);
+console.log (
+  `Soy ${firstName} ${lastName} tengo ${age} años y me gustan los lobos´ );
 
 // 1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de ambos juguetes.
 const toy1 = { name: "Buss myYear", price: 19 };
@@ -21,6 +22,7 @@ const car2 = { name: "Chevrolet Corbina", basePrice: 70000, finalPrice: 80000 };
 
 globalBasePrice = 25000;
 
-car1.finalPrice = basePrice + globalBasePrice;
-car2.finalPrice = basePrice + globalBasePrice;
-console.log(car1.finalPrice, car2.finalPrice);
+car1.finalPrice = car1.basePrice + globalBasePrice;
+car2.finalPrice = car2.basePrice + globalBasePrice;
+console.log(car1);
+console.log(car2);
